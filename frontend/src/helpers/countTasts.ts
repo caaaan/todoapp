b722f66b5fb11dce5cc7d02415a775a8b15f385a
caaaan <canborcbakan@gmail.com>
2024-08-15 17:51:16 +0300

    form yüklemeye ve database'ten dinemik olarak task'leri toplama halloldu geriye bitek todo, inprogress ve completed counter'ları kaldı

---
+++ frontend/src/helpers/countTasts.ts
@@ -0,0 +1,15 @@
+import { ITaskApi } from "../interfaces/ITaskApi"
+import { TaskCounterStatusType } from "../interfaces/ITaskCounter"
+
+export const countTasks = (tasks: ITaskApi[],status: TaskCounterStatusType): number =>{
+
+    if(!Array.isArray(tasks)){
+        return -1;
+    }
+
+    const totalTasks = tasks.filter((task)=>{
+        return task.status = status;
+    });
+
+    return totalTasks.length;
+}
